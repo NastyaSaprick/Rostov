f82ed80455b20c0e4e5dcaa87ce5232cfc90e102
NastyaSaprick <sapronovaas2002@gmail.com>
2021-04-13 18:10:45 +0300

    маска на номер телефона и доработка формы в модальном окне, подключение анимации

--- js/main.js
+++ js/main.js
@@ -30,10 +30,23 @@ $(document).ready(function () {
   modalBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
+
   closeBtn.on('click', function() {
     modal.toggleClass('modal--visible');
   });
 
+  $(document).keyup(function (keyClose) {
+    // 27 - keyCode
+    if (keyClose.which == 27 && modal.hasClass("modal--visible")) {
+        modal.toggleClass('modal--visible');
+    }
+});
+
+$(document).click(function (keyClose) {
+    if ($(keyClose.target).is('.modal')) {
+        modal.toggleClass('modal--visible')
+    }
+});
 
   /*сдайдер */
   var mySwiper = new Swiper ('.swiper-container', {
@@ -55,4 +68,37 @@ $(document).ready(function () {
   next.css('left', prev.width() + 10 + bullets.width() + 10)
   bullets.css('left', prev.width() + 10)
 
+  new WOW().init();
+
+  //валидация формы
+  $('.modal__form').validate({
+    errorClass: "invalid",
+    rules: {
+      // строчное правило
+      userName: {
+        required: true,
+        minlength: 2
+      },
+      userPhone: "required",
+      // правило - обьект (блок)
+      userEmail: {
+        required: true,
+        email: true
+      }
+    }, // сообщения
+    messages: {
+      userName:{
+        required: "Имя обязательно",
+        minlength: "Имя не короче двух букв"
+      },
+      userPhone: "Телефон обязателен",
+      userEmail: {
+        required: "Обязательно укажите Emal",
+        email: "Введите в формате: name@domain.com"
+      }
+    }
+  });
+//маска для номера
+
+$('[type=tel]').mask('+7(000)-000-00-00', { placeholder: "+7(___)-___-__-__" });
 });
