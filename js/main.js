f850c8d654cfcad83a82aa3605d90341592e67d6
NastyaSaprick <sapronovaas2002@gmail.com>
2021-04-14 16:05:52 +0300

    карта завершена

--- js/main.js
+++ js/main.js
@@ -102,4 +102,75 @@ $(document).click(function (keyClose) {
 
 $('[type=tel]').mask('+7(000)-000-00-00', { placeholder: "+7(___)-___-__-__" });
 
+//карта
+// The ymaps.ready() function will be called when
+    // all the API components are loaded and the DOM tree is generated.
+    ymaps.ready(init);
+    function init(){ 
+        // Creating the map.    
+        ymaps.ready(function () {
+          var myMap = new ymaps.Map('map', {
+                  center: [55.751574, 37.573856],
+                  zoom: 9
+              }, {
+                  searchControlProvider: 'yandex#search'
+              }),
+      
+              // Creating a content layout.
+              MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+                  '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+              ),
+      
+              myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                  hintContent: 'Наш офис',
+                  balloonContent: 'Моя метка'
+              }, {
+                  /**
+                   * Options.
+                   * You must specify this type of layout.
+                   */
+                  iconLayout: 'default#image',
+                  // Custom image for the placemark icon.
+                  iconImageHref: 'img/map.svg',
+                  // The size of the placemark.
+                  iconImageSize: [30, 40],
+                  /**
+                   * The offset of the upper left corner of the icon relative
+                   * to its "tail" (the anchor point).
+                   */
+                  iconImageOffset: [-5, -38]
+              }),
+      
+              myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
+                  hintContent: 'A custom placemark icon with contents',
+                  balloonContent: 'This one — for Christmas',
+                  iconContent: '12'
+              }, {
+                  /**
+                   * Options.
+                   * You must specify this type of layout.
+                   */
+                  iconLayout: 'default#imageWithContent',
+                  // Custom image for the placemark icon.
+                  iconImageHref: 'images/ball.png',
+                  // The size of the placemark.
+                  iconImageSize: [48, 48],
+                  /**
+                   * The offset of the upper left corner of the icon relative
+                   * to its "tail" (the anchor point).
+                   */
+                  iconImageOffset: [-24, -24],
+                  // Offset of the layer with content relative to the layer with the image.
+                  iconContentOffset: [15, 15],
+                  // Content layout.
+                  iconContentLayout: MyIconContentLayout
+              });
+      
+          myMap.geoObjects
+              .add(myPlacemark)
+              .add(myPlacemarkWithContent);
+      });
+    }
+      
+
 });
