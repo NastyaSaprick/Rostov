5268a7a7d7ee20f7d70f60b3121915dfa7f67159
NastyaSaprick <sapronovaas2002@gmail.com>
2021-04-19 15:41:25 +0300

    php в модальном окне отправка заявки

---
+++ send.php
@@ -0,0 +1,40 @@
+<?php
+
+$userName = $_POST['userName'];
+$userEmail = $_POST['userEmail'];
+$userPhone = $_POST['userPhone'];
+
+// Подключение библиотеки 
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'sapronovaas2002@gmail.com';                     // SMTP username
+    $mail->Password   = 'pass';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 465;                                    // TCP port to connect to
+
+    //Отправление письма на почту
+    $mail->setFrom('sapronovaev@mail.ru' , Настя);     //от кого отправлять
+    $mail->addAddress('sapronovaas2002@gmail.com');     // кому отправлять 
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новая заявка с сайта';
+    $mail->Body    = "Пользователь оставил данные: ${userName}, Телефон: ${userPhone}. Электронные й адрес: ${userEmail}";
+
+    $mail->send();
+    header('Location: thanks.html');
+}
+    catch (Exception $e) {
+    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+  }
